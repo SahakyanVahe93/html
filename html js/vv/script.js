8d4965b5f745950f8c658b63d7d1ee9b0b64e1ba
vahe <vahe.sahakyan.93@gmail.com>
2024-02-13 00:15:07 +0400

    urish commit

---
+++ html js/vv/script.js
@@ -0,0 +1,47 @@
+"use strict";
+/*const a = 8;
+const b = 9;
+function sum(tt, bb) {
+    return tt+bb;
+};
+console.log(sum(50,10));
+var tver = sum(2,44);
+
+const tt = document.createElement('p');
+tt.innerText = sum(11,3);
+document.getElementById('body').appendChild(tt)*/
+/* let x = "bay"
+function text(wfdt) {
+
+    return wfdt[3];
+ }
+ console.log(text("barev"));///
+   
+ function name() {
+    return 
+
+    
+ }*/
+ 
+ 
+ 
+ /*function big(one,two) {
+    if (one > two){
+         one;
+    } else if(two > one){
+         two;
+    }
+
+ }
+ console.log(big(34,24));//34
+ console.log(big(23,76));
+
+ var kk = big(11,8);//11
+ console.log(kk);*/
+
+
+ /*function text(x) {
+    return x.length
+    
+ }
+ console.log(text("bay"));*/
